e75cc45ec0d6d10c11f1e0b09b9fba26ed2114b0
Franco Davat <davat.franco@gmail.com>
2024-03-03 21:49:59 -0300

    add get /

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -4,6 +4,10 @@ import { pool } from "../db.js";
 // Crea una instancia de 'Router' para gestionar las rutas.
 const router = Router()
 
+router.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "../client/dist" });
+});
+
 // Define una ruta GET '/ping' que devuelve un JSON con el resultado de 'SELECT 1 + 1'.
 router.get('/ping', async (req,res)=>{
   try {
