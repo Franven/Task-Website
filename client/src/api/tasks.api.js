d14cfc0f044cc05238fb5b9959527de99af49e33
Franco Davat <davat.franco@gmail.com>
2024-03-04 13:23:38 -0300

    mejorando las variables de entorno

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,33 +1,35 @@
 import axios from "axios";
 
+const URL = import.meta.env.VITE_BACKEND_URL;
+
 // Función para obtener todas las tareas.
 export const getTasksRequest = async () => {
-  return await axios.get("http://localhost:4000/tasks");
+  return await axios.get(URL);
 };
 
 // Función para crear una nueva tarea.
 export const createTaskRequest = async (task) => {
-  return await axios.post("http://localhost:4000/tasks", task);
+  return await axios.post(URL, task);
 };
 
 // Función para eliminar una tarea por su ID.
 export const deleteTaskRequest = async (id) => {
-  return await axios.delete(`http://localhost:4000/tasks/${id}`);
+  return await axios.delete(`${URL}/${id}`);
 };
 
 // Función para obtener una tarea por su ID.
 export const getTaskRequest = async (id) => {
-  return await axios.get(`http://localhost:4000/tasks/${id}`);
+  return await axios.get(`${URL}/${id}`);
 };
 
 // Función para actualizar una tarea por su ID con nuevos campos.
 export const updateTaskRequest = async (id, newfields) => {
-  return await axios.put(`http://localhost:4000/tasks/${id}`, newfields);
+  return await axios.put(`${URL}/${id}`, newfields);
 };
 
 // Función para cambiar el estado de una tarea por su ID.
 export const toggleTaskDoneRequest = async (id, done) => {
-  return await axios.put(`http://localhost:4000/tasks/${id}`, {
+  return await axios.put(`${URL}/${id}`, {
     done,
   });
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { PORT } from "./config.js";
 import {dirname, join} from "path";
 import {fileURLToPath} from "url";
 
@@ -13,7 +12,7 @@ const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Configura el middleware 'cors' para permitir solicitudes desde http://localhost:5173 (frontend).
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 // Configura el middleware para parsear las solicitudes en formato JSON.
 app.use(express.json());
 // Establece las rutas para las funciones relacionadas con el índice y las tareas.
@@ -24,6 +23,6 @@ app.use(express.static(join(__dirname,'../client/dist')))
 
 // Inicia el servidor escuchando en el puerto especificado.
 
-app.listen(PORT);
+app.listen(process.env.PORT);
 
-console.log(`server esta corriendo en el puerto ${PORT}`);
+console.log(`server esta corriendo en el puerto ${process.env.PORT}`);
